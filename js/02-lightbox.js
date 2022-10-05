ce5f9f57ab4d9a8ef4457cae559253d1e4eeb58b
Adilnurlan <nurlan.adil98@gmail.com>
2022-10-05 19:59:45 +0600

    hm-7(4)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-
 const galleryItemsMarkup = document.querySelector(".gallery");
 const galleryFoo = galleryMarkup(galleryItems);
 
@@ -10,15 +9,15 @@ galleryItemsMarkup.insertAdjacentHTML("afterbegin", galleryFoo);
 function galleryMarkup(galleries) {
   return galleries
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
+      return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+</a></li>`;
     })
     .join("");
 }
 
-    let lightbox = new SimpleLightbox('.gallery a', {
-        captions: true,
-        captionsData: 'alt',
-        captionsDelay: 250,
-    });
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
